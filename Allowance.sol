e2c5f0451e6b7503ba513fb7c86ff0858f9bf804
aakash-gorai <aakashkumargorai@gmail.com>
2023-11-20 20:48:45 +0530

    add: all files added

---
+++ Allowance.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Allowance is Ownable{
+    
+    event AllowanceChanged(address indexed _byWhom, address indexed _forWhom, uint _oldAmount, uint _newAmount);
+    mapping(address => uint) public allowance;
+
+    function addAllowance (address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_msgSender(), _who, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+    constructor() Ownable(msg.sender) {}
+    function isOwner()public view returns(bool){
+        return _msgSender() == owner();
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[_msgSender()] >= _amount,"You are not allowed");
+        _;
+    }
+
+    function reduceAllowance(address _who, uint _amount) internal{
+        emit AllowanceChanged(_msgSender(), _who, allowance[_who], allowance[_who]-(_amount));
+        allowance[_who]=allowance[_who]-(_amount);
+    }
+}
